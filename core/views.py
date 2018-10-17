c226815a276150bc405e3256fe0835b858e508d0
Pithagoras Leal <39030379+pithagorasleal@users.noreply.github.com>
2018-10-17 13:44:03 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def professor_boletim(request):
-  return render request(professor_boletim.html)
+  return render(request, 'professor_boletim.html')
 
 # Create your views here.
